7e7d769f0abc23e09034d4f0ad7f988214be2690
gabcortes97 <gabriel.cortes@uchile.cl>
2025-06-18 11:27:51 -0400

    deploy

--- deploy.R
+++ deploy.R
@@ -1,6 +1,7 @@
-install.packages('rsconnect')
+library(pacman)
+p_load(rsconnect,
+       quarto)
 
 rsconnect::setAccountInfo(name='gabrielcortesp', token='8ED1A66A016F143C75BD71D230D3C0A2', secret='sQ0pJkQZepkHpzWbjLM/d3oVh9QDN2r1wET6ry+l')
 
-library(rsconnect)
-rsconnect::deployApp('.')
+quarto_publish_app(server="shinyapps.io")
