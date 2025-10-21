b88f991b44e261f1316578e76f20b03a2a97e7cf
gabcortes97 <gabriel.cortes@mail.udp.cl>
2025-10-21 20:30:01 -0300

    actualizacion vislatam

--- deploy.R
+++ deploy.R
@@ -2,6 +2,20 @@ library(pacman)
 p_load(rsconnect,
        quarto)
 
-rsconnect::setAccountInfo(name='gabrielcortesp', token='8ED1A66A016F143C75BD71D230D3C0A2', secret='sQ0pJkQZepkHpzWbjLM/d3oVh9QDN2r1wET6ry+l')
+install.packages("http://cran.r-project.org/src/contrib/Archive/curl/curl_6.2.3.tar.gz",
+                 repos = NULL, type = "source")
 
-quarto_publish_app(server="shinyapps.io")
+install.packages("devtools")
+devtools::install_github("rstudio/rsconnect")
+
+
+rsconnect::setAccountInfo(name='gabrielcortesp', token='8ED1A66A016F143C75BD71D230D3C0A2', 
+                          secret='sQ0pJkQZepkHpzWbjLM/d3oVh9QDN2r1wET6ry+l')
+
+quarto::quarto_publish_app(input = "visualizador-la.qmd",
+                   server="shinyapps.io",
+                   name = "visualizador-la",
+                   account = "gabrielcortesp")
+
+
+
